cfcfe159318caf235133f249383e314f822cd0fd
Tomy <tomyleereyes@gmail.com>
2018-04-13 15:27:42 -0700

    changed map style

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -4,6 +4,7 @@ import ReactMapGL from 'react-map-gl'
 const mapBoxToken =
   'pk.eyJ1IjoidG9teTE0MyIsImEiOiJjamZ5Z3M4YjIwMXNtMzNueHVwMGd6dTloIn0.z_yPSWapeXLaixPPUcpI-A'
 
+
 class Map extends Component {
   state = {
     viewport: {
@@ -11,14 +12,19 @@ class Map extends Component {
       height: 1000,
       latitude: 37.7577,
       longitude: -122.4376,
-      zoom: 8
+      zoom: 12
     }
   }
 
   render() {
-    return <ReactMapGL {...this.state.viewport} mapboxApiAccessToken={mapBoxToken} onViewportChange={viewport => this.setState(
-            { viewport }
-          )} />
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapStyle = {'mapbox://styles/mapbox/streets-v9'}
+        mapboxApiAccessToken={mapBoxToken}
+        onViewportChange={viewport => this.setState({ viewport })}
+      />
+    )
   }
 }
 export default Map
