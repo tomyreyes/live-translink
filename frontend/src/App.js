cffaf77bd25eb5d49fb6c3f2da0a9c0a78ce75e6
Tomy <tomyleereyes@gmail.com>
2018-04-18 15:04:53 -0700

    adding redux-form functions for api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Search/>
+        <Search onSubmit={this.log}/>
         <Map/>
         </div>
     );
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,20 +1,51 @@
 import React, { Component } from 'react'
-import { reduxForm, Field } from 'redux-form'
-
+import { reduxForm, Field, SubmissionError } from 'redux-form'
 
 
 class Search extends Component {
+  renderField = ({ label, input, meta: { touched, error } }) => (
+    <div className="input-row">
+      <label>{label}</label>
+      <input placeholder='Enter Location...'{...input} type="text" />
+      {touched && error && <span className="error">{error}</span>}
+    </div>
+  )
+
+  mySubmit = ({ location }, dispatch) => {
+    //return promise 
+    console.log(location)
+  }
+
+  //will need lifecycle method with google autocomplete here
+
   render() {
     const { handleSubmit } = this.props
     return (
-      <form onSubmit = {handleSubmit}>
-        <label>Enter Location</label>
-        <Field name="location" component="input" type="text"/>
-        </form>
+      <form onSubmit={handleSubmit(this.mySubmit)}>
+        <Field
+          name="location"
+          label={'Location'}
+          component={this.renderField}
+          type="text"
+        />
+      </form>
     )
   }
 }
+
+const validate = ({ location = '' }) => {
+  let error = {}
+  let isError = false
+  if (location.trim() === '') {
+    error.location = 'Required'
+    isError = true
+  }
+  if (isError) {
+    return error
+  }
+}
 Search = reduxForm({
-  form: 'search'
+  form: 'search', 
+  validate 
 })(Search)
 export default Search
