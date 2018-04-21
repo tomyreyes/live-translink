536d418ec21e80263b837f38cb67275c6fb59780
Tomy <tomyleereyes@gmail.com>
2018-04-21 11:57:51 -0700

    map is changing to proper coordinates!

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -7,30 +7,40 @@ const mapBoxToken =
 
 
 class Map extends Component {
-  state = {
-    viewport: {
-      width: 1600,
-      height: 1000,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 12
+  constructor() {
+    super()
+    this.state = { viewport: { width: 1600, height: 1000, latitude: 37.7577, longitude: -122.4376, zoom: 12 } }
+  }
+  
+
+  componentDidUpdate(prevProps){
+    if (prevProps !== this.props) {
+      this.setState({
+        viewport: {
+          width: 1600,
+          height: 1000,
+          latitude: this.props.mapCoordinates.lat,
+          longitude: this.props.mapCoordinates.lng,
+          zoom: 12
+        }
+      })
     }
   }
-  //currently I am listening to state changes in store
-  //need to find a way to change lat and long when that occurs
-
   render() {
+    console.log(this.props.mapCoordinates)
     return (
       <ReactMapGL
         {...this.state.viewport}
         mapStyle = {'mapbox://styles/mapbox/streets-v9'}
         mapboxApiAccessToken={mapBoxToken}
-        onViewportChange={viewport => this.setState({ viewport })}
+        // onViewportChange={viewport => this.setState({ viewport })}
       />
     )
   }
 }
 const mapStateToProps = state => {
-  console.log(state)
+  return {
+    mapCoordinates: state.mapCoordinates
+  }
 }
 export default connect(mapStateToProps)(Map)
