b3f98a001ce406177ad0b0b8d87b937007ce7719
Tomy <tomyleereyes@gmail.com>
2018-04-22 12:23:36 -0700

    finished sandbox

--- frontend/src/sandbox.js
+++ frontend/src/sandbox.js
@@ -2,31 +2,22 @@ const request = require('superagent')
 
 const key = 'S6i8bxWApJSAiWqp7Xwr'
 
-// request
-//   .get('http://api.translink.ca/rttiapi/v1/stops')
-//   .set('Content-Type', 'application/json')
-//   .query({ 
-//     apiKey: key, 
-//     lat: 49.61, 
-//     long: -123.01, 
-//     radius: 2000
-//   })
-//   .then(res => {
-//     console.log(res)
-//   }).catch(error => {
-//     console.log(error)
-//   })
-  
-request
+let results = 
+  request
   .get(
     'http://api.translink.ca/rttiapi/v1/stops?'
   )
   .set('Content-Type', 'application/json')
-  .query({apiKey: key, lat: 49.187706, long: -122.850060 })
+  .query({apiKey: key, lat: 49.187706, long: -122.850060, radius: 500 })
   .then(res => {
-    console.log(res.text)
+    console.log(res.body)
   })
   .catch(error => {
     console.log(error)
   })
+  
+  // I can iterate through this using forEach and push coordinates of each bus stop to an array 
+  //this array will be sent as payload to store 
+  //from this I can mapStateToProps -> coordinatesArr.map -> show results on map 
+
   
